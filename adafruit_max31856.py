582ef48e34aef5c2fe9f160064132340645bb450
Eirinn Mackay <eirinn.mackay@micrographiabio.com>
2022-08-02 16:26:17 +0100

    fix formatting with black

--- adafruit_max31856.py
+++ adafruit_max31856.py
@@ -170,7 +170,7 @@ class MAX31856:
         return self.unpack_temperature()
 
     def unpack_temperature(self) -> float:
-        '''Reads the probe temperature from the register'''
+        """Reads the probe temperature from the register"""
         # unpack the 3-byte temperature as 4 bytes
         raw_temp = unpack(
             ">i", self._read_register(_MAX31856_LTCBH_REG, 3) + bytes([0])
@@ -184,8 +184,6 @@ class MAX31856:
 
         return temp_float
 
-
-
     @property
     def reference_temperature(self):
         """Wait to retreive temperature of the cold junction in degrees Celsius. (read-only)"""
@@ -193,7 +191,7 @@ class MAX31856:
         return self.unpack_reference_temperature()
 
     def unpack_reference_temperature(self) -> float:
-        '''Reads the reference temperature from the register'''
+        """Reads the reference temperature from the register"""
         raw_read = unpack(">h", self._read_register(_MAX31856_CJTH_REG, 2))[0]
 
         # effectively shift raw_read >> 8 to convert pseudo-float
@@ -277,13 +275,12 @@ class MAX31856:
         # wait for the measurement to complete
         self._wait_for_oneshot()
 
-
     def initiate_one_shot_measurement(self):
-        '''Starts a one-shot measurement and returns immediately.
+        """Starts a one-shot measurement and returns immediately.
         A measurement takes approximately 160ms.
         Check the status of the measurement with `oneshot_pending`; when it is false,
         the measurement is complete and the value can be read with `unpack_temperature`.
-        '''
+        """
         # read the current value of the first config register
         conf_reg_0 = self._read_register(_MAX31856_CR0_REG, 1)[0]
 
@@ -295,19 +292,19 @@ class MAX31856:
         # write it back with the new values, prompting the sensor to perform a measurement
         self._write_u8(_MAX31856_CR0_REG, conf_reg_0)
 
-
     @property
     def oneshot_pending(self) -> bool:
-        '''A boolean indicating the status of the one-shot flag.
-            A True value means the measurement is still ongoing.
-            A False value means measurement is complete.'''
-        oneshot_flag = self._read_register(_MAX31856_CR0_REG, 1)[0] & _MAX31856_CR0_1SHOT
+        """A boolean indicating the status of the one-shot flag.
+        A True value means the measurement is still ongoing.
+        A False value means measurement is complete."""
+        oneshot_flag = (
+            self._read_register(_MAX31856_CR0_REG, 1)[0] & _MAX31856_CR0_1SHOT
+        )
         return bool(oneshot_flag)
 
     def _wait_for_oneshot(self):
         while self.oneshot_pending:
             sleep(0.01)
-
 
     def _read_register(self, address, length):
         # pylint: disable=no-member
